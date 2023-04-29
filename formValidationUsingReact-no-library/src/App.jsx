f16b7be7da895fae11cc8e5cc98c62c8fc15e2ef
Prakash Timilsina <prakashtimilsina84@gmail.com>
2023-04-29 08:36:00 -0500

    handle focused added

--- formValidationUsingReact-no-library/src/App.jsx
+++ formValidationUsingReact-no-library/src/App.jsx
@@ -10,50 +10,58 @@ const initialValue = {
   confirmPassword: "",
 };
 
-const inputs = [
-  {
-    id: 1,
-    name: "username",
-    type: "text",
-    placeholder: "User Name",
-    errorMessage: "Username should be 3-16 characters and should not include any special characters.",
-    label: "User Name",
-  },
-  {
-    id: 2,
-    name: "email",
-    type: "email",
-    placeholder: "Email",
-    errorMessage: "It should be a valid email",
-    label: "Email",
-  },
-  {
-    id: 3,
-    name: "birthday",
-    type: "date",
-    placeholder: "Birthday",
-    label: "Birthday",
-  },
-  {
-    id: 4,
-    name: "password",
-    type: "password",
-    placeholder: "Password",
-    errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
-    label: "Password",
-  },
-  {
-    id: 5,
-    name: "confirmPassword",
-    type: "password",
-    placeholder: "Confirm Password",
-    errorMessage: "Password donot match.",
-    label: "Confirm Password",
-  },
-];
 
 function App() {
   const [values, setValues] = useState(initialValue);
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "User Name",
+      errorMessage: "Username should be 3-16 characters and should not include any special characters.",
+      label: "User Name",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
+      label: "Password",
+      pattern: "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*_+])[A-Za-z0-9!@#$%^&*_+]{8,20}$",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Password donot match.",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];  
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- formValidationUsingReact-no-library/src/components/FormInput.jsx
+++ formValidationUsingReact-no-library/src/components/FormInput.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./formInput.css";
+import { useState } from "react";
 
 function FormInput(props) {
-const { label, errorMessage, onChange, id, ...inputProps} = props;
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocuse = () => {
+    setFocused(true);
+  };
 
   return (
     <>
       <div className="formInput">
         <label>{label}</label>
-        <input {...inputProps} onChange={onChange} />
+        <input
+          {...inputProps}
+          onChange={onChange}
+          onBlur={handleFocuse}
+          onFocus={()=>inputProps.name==="confirmPassword" && setFocused('true')}
+          focused={focused.toString()}
+        />
         <span>{errorMessage}</span>
       </div>
     </>
